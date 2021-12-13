ec79dedc4f864543122b2ad1bcc94c34539d553c
josh <joshowillmott@outlook.com>
2021-12-13 11:14:47 +0000

    ALP-34

--- Achievement Project/Scripts/index.js
+++ Achievement Project/Scripts/index.js
@@ -5,16 +5,21 @@ const getOutput = document.querySelector("#Output");
 document.querySelector("#UserForm").addEventListener("submit", function (event) {
     event.preventDefault(); 
     const form = this;
-
     const data = {
     achivementName: form.name.value,
     description: form.name.value, 
     achievementValue: form.name.value,
     achievementUnlocked: form.name.value,
-
-
 };
 console.log("DATA: ", data);
 form.reset();
 form.userId.focus();
-});
+console.log("Hello");
+
+
+axios.post("http://localhost:8080/create", data)
+.then(res => console.log(res))
+.catch(err => console.error(err));
+    })
+        .catch(err => console.error(err));
+

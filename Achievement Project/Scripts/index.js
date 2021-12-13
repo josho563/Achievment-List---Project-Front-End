86a72910fd2cbc27e25d31af5332f923f6d631e2
josh <joshowillmott@outlook.com>
2021-12-13 10:30:19 +0000

    ALP-26

--- Achievement Project/Scripts/index.js
+++ Achievement Project/Scripts/index.js
@@ -1,4 +1,20 @@
 "use strict";
 
-//List All users
+//Create
 const getOutput = document.querySelector("#Output");
+document.querySelector("#UserForm").addEventListener("submit", function (event) {
+    event.preventDefault(); 
+    const form = this;
+
+    const data = {
+    achivementName: form.name.value,
+    description: form.name.value, 
+    achievementValue: form.name.value,
+    achievementUnlocked: form.name.value,
+
+
+};
+console.log("DATA: ", data);
+form.reset();
+form.userId.focus();
+});
